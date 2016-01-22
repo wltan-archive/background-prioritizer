a616171717b3bc1f9f12c1b45a270f93252d1683
iPokeU <ianyongasd@gmail.com>
2016-01-22 12:40:40 +0800

    Toolbar working now

--- app/src/main/java/sg/edu/nushigh/arp/backgroundprioritizer/MainActivity.java
+++ app/src/main/java/sg/edu/nushigh/arp/backgroundprioritizer/MainActivity.java
@@ -1,10 +1,17 @@
 package sg.edu.nushigh.arp.backgroundprioritizer;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
@@ -12,6 +19,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+	private Toolbar toolbar;
 	static boolean on;
 	Button toggle;
 	NumberPicker priopick;
@@ -21,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		toolbar = (Toolbar) findViewById(R.id.toolbar); //attaching the layout to the toolbar object
+		setSupportActionBar(toolbar);
 
 		toggle = (Button) findViewById(R.id.onoff);
 		priopick = (NumberPicker) findViewById(R.id.priopick);
@@ -34,6 +44,35 @@ public class MainActivity extends AppCompatActivity {
 
 		on = (PriorityChangeService.instance != null);
 		toggle.setText(on?"Deactivate Service":"Activate Service");
+
+		//code to color status bar for >= lollipop
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			Window window = getWindow();
+			window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+			window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 
 	private static final int MIN_POLLTIME = 1000;
