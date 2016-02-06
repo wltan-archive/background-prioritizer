1938fdd816f0e7b98a1fe6645a2302617c636a5a
iPokeU <ianyongasd@gmail.com>
2016-02-07 01:11:37 +0800

    UI update loop not working

--- app/src/main/java/sg/edu/nushigh/arp/backgroundprioritizer/Tab3.java
+++ app/src/main/java/sg/edu/nushigh/arp/backgroundprioritizer/Tab3.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.concurrent.ScheduledExecutorService;
@@ -15,30 +16,35 @@ import java.util.concurrent.TimeUnit;
 public class Tab3 extends Fragment {
 
     View v;
-    TextView android, uptime, imei, model, rooted;
+    TextView android, uptime, imei, model, rooted, wifi;
+    ImageView image_wifi;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         v = inflater.inflate(R.layout.tab_3, container, false);
 
-        SystemInfo si = new SystemInfo(getContext());
+        final SystemInfo si = new SystemInfo(getContext());
 
         android = (TextView) v.findViewById(R.id.value_android);
         android.setText(si.androidVersionCode() + " (" + si.androidVersionName() + ")");
         uptime = (TextView) v.findViewById(R.id.value_uptime);
-        uptime.setText(si.uptime());
         imei = (TextView) v.findViewById(R.id.value_imei);
         imei.setText(si.imei());
         model = (TextView) v.findViewById(R.id.value_model);
         model.setText(si.brand() + " " + si.model());
         rooted = (TextView) v.findViewById(R.id.value_rooted);
         rooted.setText(""); //TODO
+        wifi = (TextView) v.findViewById(R.id.value_wifi);
+        image_wifi = (ImageView) v.findViewById(R.id.image_wifi);
 
         Runnable update = new Runnable(){
             @Override
             public void run(){
                 // update stuff here
                 // remember that UI things have to be declared final before accessing here (for some reason)
+                uptime.setText(si.uptime());
+                wifi.setText(si.wifiConnected() ? "On" : "Off");
+                image_wifi.setImageResource(si.wifiConnected() ? R.drawable.ic_info_wifi : R.drawable.ic_info_wifi_off);
             }
         };
 
